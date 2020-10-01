e095ed00f20e0afc390857828f3ac64f230c1011
Deborah-code <dbrhadewunmi@gmail.com>
2020-10-01 16:07:23 +0100

    made a few changes to the remarks

--- bmi_calc.py
+++ bmi_calc.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import messagebox
+import random
 
 Screen = Tk()
 
@@ -7,6 +8,21 @@ Screen.geometry("400x400")
 Screen.configure(background="#8B008B")
 Screen.title("BMI Calculator")
 Screen.resizable(width=False, height=False)
+
+extremely_underweight = ["Eat Some MEAT and more MEAT hunnay!!", "Please dear add some height or weight"]
+i = random.choice(extremely_underweight)
+very_underweight = ["It's okay to eat yunno !", "Food is not poison, eat it!", "Are you scared of food?"]
+j = random.choice(very_underweight)
+underweight = ["Okay boo, we need some pounds, just a little more!", "Youre actually almost there", "Just eat a little more"]
+k = random.choice(underweight)
+normal = ["Okay you're fine, keep moving", " Damn boo, that body is banging!!!.", "Teach us your ways, we want to be like you"]
+l = random.choice(normal)
+overweight = ["Please register at a gymn.", "Its like you need to start fasting", "Avoid food dear"]
+m = random.choice(overweight)
+very_overweight = ["You need an entire lifestyle change \n you cannot continue like this o!", "Stop eating", "stay away from anything that looks edible"]
+n = random.choice(very_overweight)
+extremely_overweight = ["I'm pretty sure you cant move again \n is this the lifestyle you have chosen?", "You should consider buying a gym", "Its only Jesus that can help you"]
+o = random.choice(extremely_overweight)
 
 def get_height():
     '''
@@ -37,25 +53,25 @@ def calculate_bmi():
         messagebox.showinfo("Result", "Please enter valid data!")
     else:
         if bmi <= 15.0:
-            res =  name + "Your BMI is " + str(bmi) + name + "\nRemarks: Eat Some MEAT and more MEAT hunnay!!" 
+            res =  "Your BMI is " + str(bmi) + "\nRemarks: " + i
             messagebox.showinfo("Result", res)
         elif bmi > 15.0 and bmi <= 16.0:
-            res = "Your BMI is " + str(bmi) + "\nRemarks: It's okay to eat yunno !"
+            res = "Your BMI is " + str(bmi) + "\nRemarks: " + j
             messagebox.showinfo("Result", res)
         elif bmi > 16.0 and bmi < 18.5:
-            res = "Your BMI is " + str(bmi) + "\nRemarks: Okay boo, we need some pounds, just a little more!"
+            res = "Your BMI is " + str(bmi) + "\nRemarks: "+ k
             messagebox.showinfo("Result", res)
         elif bmi >= 18.5 and bmi <= 25.0:
-            res = "Your BMI is " + str(bmi) + "\nRemarks: Damn boo, that body is banging!!!."
+            res = "Your BMI is " + str(bmi) + "\nRemarks: " + l
             messagebox.showinfo("Result", res)
         elif bmi > 25.0 and bmi <= 30:
-            res = "Your BMI is " + str(bmi) + "\nRemarks: Please register at a gymn."
+            res = "Your BMI is " + str(bmi) + "\nRemarks: " + m
             messagebox.showinfo("Result", res)
         elif bmi > 30.0 and bmi <= 35.0:
-            res = "Your BMI is " + str(bmi) + "\n\nRemarks: You need an entire lifestyle change \n you cannot continue like this o!"
+            res = "Your BMI is " + str(bmi) + "\n\nRemarks: " + n
             messagebox.showinfo("Result", res)
         elif bmi > 35.0 and bmi <= 40.0:
-            res = "Your BMI is " + str(bmi) + "\nRemarks: You need to buy a gym!"
+            res = "Your BMI is " + str(bmi) + "\nRemarks: " + o 
             messagebox.showinfo("Result", res)
         else:
             res = "Your BMI is " + str(bmi) + "\nRemarks: Ahh Its over dear, it is!!"
